a6e9f1936053da65e97f79ae6eb643c47b6da58a
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2024-07-22 07:22:48 +0300

    -create function in the core utils for calcualte how many times you need for reading this blog -create card for display the blogs in the home screen and make the color for each one different

--- lib/featrures/blog/presentation/screens/blog_screen.dart
+++ lib/featrures/blog/presentation/screens/blog_screen.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_clean_architecture/core/common/widgets/loader.dart';
+import 'package:flutter_clean_architecture/core/theme/app_palette.dart';
+import 'package:flutter_clean_architecture/core/utils/show_snackbar.dart';
+import 'package:flutter_clean_architecture/featrures/blog/presentation/bloc/blog_bloc.dart';
 import 'package:flutter_clean_architecture/featrures/blog/presentation/screens/add_new_blog_screen.dart';
+import 'package:flutter_clean_architecture/featrures/blog/presentation/widgets/blog_card.dart';
 
-class BlogScreen extends StatelessWidget {
+class BlogScreen extends StatefulWidget {
   static route() => MaterialPageRoute(builder: (context) => const BlogScreen());
 
   const BlogScreen({super.key});
+
+  @override
+  State<BlogScreen> createState() => _BlogScreenState();
+}
+
+class _BlogScreenState extends State<BlogScreen> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<BlogBloc>().add(BlogFetchAllBlogs());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +39,35 @@ class BlogScreen extends StatelessWidget {
             ),
           )
         ],
+      ),
+      body: BlocConsumer<BlogBloc, BlogState>(
+        listener: (context, state) {
+          if (state is BlogFailure) {
+            showSnackBar(context, state.error);
+          }
+        },
+        builder: (context, state) {
+          if (state is BlogLoading) {
+            return const Loader();
+          }
+          if (state is BlogDisplaySuccess) {
+            return ListView.builder(
+              itemCount: state.blogs.length,
+              itemBuilder: (context, index) {
+                final blog = state.blogs[index];
+                return BlogCard(
+                  blog: blog,
+                  color: index % 3 == 0
+                      ? AppPalette.gradient1
+                      : index % 3 == 1
+                          ? AppPalette.gradient2
+                          : AppPalette.gradient3,
+                );
+              },
+            );
+          }
+          return const SizedBox();
+        },
       ),
     );
   }
